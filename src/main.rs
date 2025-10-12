cbc383abf8ccc7339656f5c616edcefe0a8a2dfa
Jonir Rings <i@jonirrings.com>
2025-10-12 20:37:31 +0800

    feat: split into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
 use clap::Parser;
-use cli::Cli;
 use color_eyre::Result;
-
-use crate::app::App;
-
-mod action;
-mod app;
-mod cli;
-mod components;
-mod config;
-mod errors;
-mod logging;
-mod tui;
+use hello_rat::app::App;
+use hello_rat::cli::Cli;
+use hello_rat::{errors, logging};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    crate::errors::init()?;
-    crate::logging::init()?;
+    errors::init()?;
+    logging::init()?;
 
     let args = Cli::parse();
     let mut app = App::new(args.tick_rate, args.frame_rate)?;
